3a8c0cebb808c4ef25669c4cbfab3dd5e13eb88f
ThDat131 <truongthanhdat2002@gmail.com>
2024-11-06 19:48:59 +0700

    Add toast message

--- src/app/(admin)/admin/layout.tsx
+++ src/app/(admin)/admin/layout.tsx
@@ -3,6 +3,7 @@ import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import getDashboardTheme from './dashboard/theme/getDashboardTheme';
+import { ToastContainer } from 'react-toastify';
 
 export default function RootLayout({
     children,
@@ -18,6 +19,7 @@ export default function RootLayout({
                     {children}
                 </LocalizationProvider>
             </ThemeProvider>
+            <ToastContainer />
         </main>
     );
 }
